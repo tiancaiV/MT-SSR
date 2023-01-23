df880f1ab039896cb5b44d39d4f97b419bfa3cf7
tiancaiV <wifiway@163.com>
2023-01-23 21:05:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,13 @@ passwall)
 	opkg install luci-i18n-base-zh-cn luci-app-passwall  luci-compat luci-lib-ipkg
 	;;
 openclash)
-	wget -qO- https://cdn.jsdelivr.net/gh/tiancaiV/Install-Openclash-for-MT2500/install_clash.sh | sh
+        opkg install dnsmasq-full --download-only && opkg remove dnsmasq && opkg install dnsmasq-full --cache . && rm *.ipk
+
+	opkg install wget-ssl coreutils-nohup bash iptables curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun luci-compat libcurl4
+
+	opkg install luci-app-openclash
+
+	#wget -qO- https://cdn.jsdelivr.net/gh/tiancaiV/Install-Openclash-for-MT2500/install_clash.sh | sh
 *)
 	opkg install luci-i18n-base-zh-cn luci-app-passwall  luci-app-ssr-plus  luci-compat luci-lib-ipkg
 	wget -qO- https://cdn.jsdelivr.net/gh/tiancaiV/Install-Openclash-for-MT2500/install_clash.sh | sh

e175010599ef9cf08bb039bd86fc0c1b18e9a2bc
tiancaiV <wifiway@163.com>
2023-01-26 21:11:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,30 +4,18 @@ echo "src/gz custom_feeds https://op.supes.top/packages/aarch64_cortex-a53" >>/e
 opkg update
 action="$1"
 case "${action}" in
-ssr-plus)
-	opkg install luci-i18n-base-zh-cn luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn luci-compat luci-lib-ipkg
+ssr)
+	opkg install luci-i18n-base-zh-cn luci-app-ssr-plus  luci-compat luci-lib-ipkg luci-i18n-base-zh-cn
 	;;
-passwall)
-	opkg install luci-i18n-base-zh-cn luci-app-passwall luci-i18n-passwall-zh-cn luci-compat luci-lib-ipkg
-	;;
-openclash)
-	#opkg install dnsmasq-full --download-only && opkg remove dnsmasq && opkg install dnsmasq-full --cache . && rm *.ipk
 
-	#opkg install wget-ssl coreutils-nohup bash iptables curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun luci-compat libcurl4
-
-	#opkg install luci-app-openclash
-	
-	wget -qO- https://cdn.jsdelivr.net/gh/tiancaiV/Install-Openclash-for-MT2500/install.sh | sh
-	
-	;;
 common)
 	opkg install luci-i18n-base-zh-cn luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn luci-compat luci-lib-ipkg luci-i18n-aliyundrive-webdav-zh-cn luci-app-serverchan luci-i18n-aliddns-zh-cn luci-i18n-vlmcsd-zh-cn luci-i18n-upnp-zh-cn luci-i18n-smartdns-zh-cn luci-i18n-ramfree-zh-cn luci-i18n-diskman-zh-cn
 	;;
 *)
-	opkg install luci-i18n-base-zh-cn luci-app-passwall luci-i18n-passwall-zh-cn luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn luci-compat luci-lib-ipkg
+	opkg install luci-i18n-base-zh-cn  luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn luci-compat luci-lib-ipkg
 	wget -qO- https://cdn.jsdelivr.net/gh/tiancaiV/Install-Openclash-for-MT2500/install.sh | sh
 	;;
 esac
 
 
-#wget -qO- https://cdn.jsdelivr.net/gh/tiancaiV/MT-SSR/install.sh | sh -s ssr-plus
+#wget -qO- https://cdn.jsdelivr.net/gh/tiancaiV/MT-SSR/install.sh | sh -s ssr
